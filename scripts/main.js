d7cfd3b66cca3cc5026872c457e2493c9addb3ad
Dane du Plessis <daneasc@gmail.com>
2020-01-07 20:49:59 +0200

    ok can keep track of numbers

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,18 @@
 function callFunc () {
     console.log('Rolling');
+    window.number_of_dice = 1;
+    console.log(window.number_of_dice);
+}
 
-    
+function addDi() {
+    window.number_of_dice = window.number_of_dice + 1;
+    console.log(window.number_of_dice);
+}
 
+function removeDi() {
+    var n = window.number_of_dice;
+    if(n > 1) {
+        window.number_of_dice = n-1;
+    }
+    console.log(window.number_of_dice);
 }

0920f57cc3c11b4b7abbcaa07b89f352aa15fcc6
Dane du Plessis <daneasc@gmail.com>
2020-01-08 20:14:55 +0200

    added one displayed di

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,4 @@
-(function init() {
-    setNumberOfDice(1);
-})();
+setNumberOfDice(1);
 
 function getNumberOfDice() {
     return localStorage.getItem('numberOfDice');
@@ -42,7 +40,28 @@ function renderDice() {
     // show everything in the array of numbers.
     var element = document.querySelector("#DICE_HERE");
     element.innerHTML = randoms.join("  ");
+    drawDice(window.dice[0]);
 }
 
+function drawDice(n) {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'red';
+    
+    // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+    const w = 111;
+    const gap = 4;
+    let dx = w * (n-1) + gap * (n-1);
+    if(n == 5|| n == 6) {
+        dx += 2;
+    }
+
+    ctx.drawImage(document.getElementById('source'),
+                11 + dx, 6+w+gap, 
+                w, w, 
+                0, 0, 
+                w, w);
+}
 
 // https://getbutterfly.com/generate-html-list-from-javascript-array/
